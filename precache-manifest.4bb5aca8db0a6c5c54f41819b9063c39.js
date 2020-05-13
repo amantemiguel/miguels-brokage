e7895a18a3e23041578d7f87ddd6f06e449f18a2
amantemiguel <61431200+amantemiguel@users.noreply.github.com>
2020-05-13 11:46:53 -0700

    Updates

---
+++ precache-manifest.4bb5aca8db0a6c5c54f41819b9063c39.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a42a22d3a9aa60eb9667fb665c13d2",
+    "url": "/miguels-brokerage/index.html"
+  },
+  {
+    "revision": "36b6641c78c71c493bf5",
+    "url": "/miguels-brokerage/static/css/10.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "ac0f59037649133fab19",
+    "url": "/miguels-brokerage/static/css/11.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "58a80a7b210b6be99a7d",
+    "url": "/miguels-brokerage/static/css/12.1d9a2034.chunk.css"
+  },
+  {
+    "revision": "be18e29d2c51bda0ef19",
+    "url": "/miguels-brokerage/static/css/2.21d3525d.chunk.css"
+  },
+  {
+    "revision": "107cd345fdd7f959b4c7",
+    "url": "/miguels-brokerage/static/css/6.0d2b98a0.chunk.css"
+  },
+  {
+    "revision": "75e457e38f66fcbe7e40",
+    "url": "/miguels-brokerage/static/css/7.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "248368d9e02bfc781a0c",
+    "url": "/miguels-brokerage/static/css/8.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "fe4ebbc9f7b4cb6e518c",
+    "url": "/miguels-brokerage/static/css/9.37c8f0ee.chunk.css"
+  },
+  {
+    "revision": "a6bbb520be1089e8fac7",
+    "url": "/miguels-brokerage/static/css/main.8390ce8f.chunk.css"
+  },
+  {
+    "revision": "0894d23314f67968731a",
+    "url": "/miguels-brokerage/static/js/0.a5830166.chunk.js"
+  },
+  {
+    "revision": "5a3fea8fa1ce81a4b534",
+    "url": "/miguels-brokerage/static/js/1.f628bb20.chunk.js"
+  },
+  {
+    "revision": "36b6641c78c71c493bf5",
+    "url": "/miguels-brokerage/static/js/10.c1a77b67.chunk.js"
+  },
+  {
+    "revision": "ac0f59037649133fab19",
+    "url": "/miguels-brokerage/static/js/11.e7266b34.chunk.js"
+  },
+  {
+    "revision": "58a80a7b210b6be99a7d",
+    "url": "/miguels-brokerage/static/js/12.5d4bca0b.chunk.js"
+  },
+  {
+    "revision": "3fe54621113f40fd3deb",
+    "url": "/miguels-brokerage/static/js/13.d7f7bd21.chunk.js"
+  },
+  {
+    "revision": "ceb6c80c00ff944016d4",
+    "url": "/miguels-brokerage/static/js/14.23368903.chunk.js"
+  },
+  {
+    "revision": "be18e29d2c51bda0ef19",
+    "url": "/miguels-brokerage/static/js/2.efae3a89.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/miguels-brokerage/static/js/2.efae3a89.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e69aa05ddf7a4336f24",
+    "url": "/miguels-brokerage/static/js/3.a8d29266.chunk.js"
+  },
+  {
+    "revision": "362cd232e6b400756e3bc1723069a8cd",
+    "url": "/miguels-brokerage/static/js/3.a8d29266.chunk.js.LICENSE"
+  },
+  {
+    "revision": "107cd345fdd7f959b4c7",
+    "url": "/miguels-brokerage/static/js/6.41c11bfa.chunk.js"
+  },
+  {
+    "revision": "e89ad3dfb38ac973fcf2df05f27946de",
+    "url": "/miguels-brokerage/static/js/6.41c11bfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75e457e38f66fcbe7e40",
+    "url": "/miguels-brokerage/static/js/7.bc412760.chunk.js"
+  },
+  {
+    "revision": "248368d9e02bfc781a0c",
+    "url": "/miguels-brokerage/static/js/8.fe6fbc3a.chunk.js"
+  },
+  {
+    "revision": "fe4ebbc9f7b4cb6e518c",
+    "url": "/miguels-brokerage/static/js/9.6ff83afc.chunk.js"
+  },
+  {
+    "revision": "a6bbb520be1089e8fac7",
+    "url": "/miguels-brokerage/static/js/main.e1b85723.chunk.js"
+  },
+  {
+    "revision": "e5d096f9e9d548643f0a",
+    "url": "/miguels-brokerage/static/js/runtime-main.ec638f9f.js"
+  },
+  {
+    "revision": "8c20481d522a1bf8c654d2fcb5d769b2",
+    "url": "/miguels-brokerage/static/media/bribe.8c20481d.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/miguels-brokerage/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/miguels-brokerage/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/miguels-brokerage/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/miguels-brokerage/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/miguels-brokerage/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
